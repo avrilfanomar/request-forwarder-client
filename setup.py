6f963582964b57a722d742334ea56f5b9ad130a2
Andrii Andriichuk <andrey000mar@gmail.com>
2025-05-17 22:19:16 +0200

    add override env variables

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="request-forwarder-client",
-    version="1.0.1",
+    version="1.1.0",
     packages=find_packages(),
     install_requires=[
         "paho-mqtt",
